99b679b7c5cefe98a37b6863c2e30d3cf036a6da
rama <rama@hammer.com>
2017-08-20 22:23:43 +0930

    Copyright notice added.

--- react/sliderule_construction_kit.js
+++ react/sliderule_construction_kit.js
@@ -1,3 +1,24 @@
+///////////////////////////////////////////////////////////////////////////////////
+//                       Copyright (C) 2017 Robert P. Wolf                       //
+//                                                                               //
+// Permission is hereby granted, free of charge, to any person obtaining a copy  //
+// of this software and associated documentation files (the "Software"), to deal //
+// in the Software without restriction, including without limitation the rights  //
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     //
+// copies of the Software, and to permit persons to whom the Software is         //
+// furnished to do so, subject to the following conditions:                      //
+//                                                                               //
+// The above copyright notice and this permission notice shall be included in    //
+// all copies or substantial portions of the Software.                           //
+//                                                                               //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    //
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      //
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   //
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        //
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, //
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     //
+// THE SOFTWARE.                                                                 //
+///////////////////////////////////////////////////////////////////////////////////
 
 var roundRect = function (ctx, left1, left2, top, right1, right2, bottom, radius) {
   var atan = Math . atan2 (left1 - left2, bottom - top - radius - radius);
@@ -425,7 +446,6 @@ var draw_sine_cosine_dec = function (ctx, length, height, scale, type) {
   shifter = - shifter;
   ctx . fillStyle = scale . alt;
   for (var degree = 20; degree <= 80; degree += 10) mmark (ctx, degree, length * fn_sin_dec (90 - degree) + shifter, h5);
-  mmark (ctx, 20, length * fn_sin_dec (70), h5);
   draw_XR (ctx, fn_sin_dec, length, 60, 80, 1, h3, 10, 5, 10);
   draw_XR (ctx, fn_sin_dec, length, 60, 80, 1, h2, 5, 1, 5);
   draw_XR (ctx, fn_sin_dec, length, 20, 60, 1, h4, 10, 5, 10);
