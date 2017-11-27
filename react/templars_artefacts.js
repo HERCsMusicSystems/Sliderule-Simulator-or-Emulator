60dd9427c6924f1a2978ba9f382b9df917d73f0b
rama <rama@hammer.com>
2017-11-27 21:42:38 +1030

    Cosmetic corrections.

--- react/templars_artefacts.js
+++ react/templars_artefacts.js
@@ -15,3 +15,15 @@ var updateArtefacts = function () {
 	document . getElementById ('artefacts') . innerHTML = content . join ('<br />');
 };
 
+var updateSeeker = function () {
+	var a = artefacts ();
+	var el = document . getElementById ('seeker');
+	switch (a . length) {
+	case 0: case 1: el . innerHTML = "The poor seeker of ancient artefacts."; break;
+	case 2: el . innerHTML = "Ancient artefact's Entered Apprentice."; break;
+	case 3: el . innerHTML = "Ancient artefact Fellow."; break;
+	case 4: el . innerHTML = "Ancient artefact Master."; break;
+	default: el . innerHTML = "Temple Builder."; break;
+	}
+};
+
