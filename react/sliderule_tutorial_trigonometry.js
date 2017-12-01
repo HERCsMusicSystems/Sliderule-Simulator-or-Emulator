a60781b06704a016bc35eac73cdae9b76c9e1381
robert <robert@haesemathematics.com.au>
2017-12-01 14:59:42 +1030

    C/D corrections.

--- react/sliderule_tutorial_trigonometry.js
+++ react/sliderule_tutorial_trigonometry.js
@@ -6,14 +6,14 @@ trigonometryLessons ['Sine of an angle'] = function (message) {
   while (angle > 80 || angle < 6) angle = crnu (Math . floor (Math . random () * 100));
   var sine = crnu (Math . sin (angle * Math . PI / 180), 6);
   return [
-  {action: function () {ensureSide (["S", "C"]);}, delay: 100},
+  {action: function () {ensureSide (["S", "D"]);}, delay: 100},
   {action: function () {message ("The task: find sine of " + angle + " degrees.");}, delay: 500},
-  {action: function () {isolate (["S", "C"]), changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 500},
+  {action: function () {isolate (["S", "D"]), changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 500},
   {action: function () {message ("Move the cursor to " + angle + " on the S scale.");}, delay: 3000},
   {action: function () {cursorTo ("S", angle);}, delay: 3000},
-  {action: function () {message ("Read the value of " + crnu (sine * 10) + " on the C scale.");}, delay: 3000},
+  {action: function () {message ("Read the value of " + crnu (sine * 10) + " on the D scale.");}, delay: 3000},
   {action: function () {message ("Adjust the decimal point to obtain value of " + crnu (sine, 5) + ".");}, delay: 2000},
-  {action: function () {cursorTo ("C", 1); dimmm (80, 255, 8);
+  {action: function () {cursorTo ("D", 1); dimmm (80, 255, 8);
   sliderules . objective = function () {
     if (checkValue ("S", angle)) {message ("Mission accomplished!"); increaseCookieResult ('Sine of an angle'); return true;}
     return false;
@@ -27,14 +27,14 @@ trigonometryLessons ['Tangent of an angle'] = function (message) {
   while (angle > 45 || angle < 6) angle = crnu (Math . floor (Math . random () * 100));
   var tangent = crnu (Math . tan (angle * Math . PI / 180), 6);
   return [
-  {action: function () {ensureSide (["T", "C"]);}, delay: 100},
+  {action: function () {ensureSide (["T", "D"]);}, delay: 100},
   {action: function () {message ("The task: find tangent of " + angle + " degrees.");}, delay: 500},
-  {action: function () {isolate (["T", "C"]), changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 500},
+  {action: function () {isolate (["T", "D"]), changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 500},
   {action: function () {message ("Move the cursor to " + angle + " on the T scale.");}, delay: 3000},
   {action: function () {cursorTo ("T", angle);}, delay: 3000},
-  {action: function () {message ("Read the value of " + crnu (tangent * 10) + " on the C scale.");}, delay: 3000},
+  {action: function () {message ("Read the value of " + crnu (tangent * 10) + " on the D scale.");}, delay: 3000},
   {action: function () {message ("Adjust the decimal point to obtain value of " + crnu (tangent, 5) + ".");}, delay: 2000},
-  {action: function () {cursorTo ("C", 1); dimmm (80, 255, 8);
+  {action: function () {cursorTo ("D", 1); dimmm (80, 255, 8);
   sliderules . objective = function () {
     if (checkValue ("T", angle)) {message ("Mission accomplished!"); increaseCookieResult ('Tangent of an angle'); return true;}
     return false;
@@ -48,15 +48,15 @@ trigonometryLessons ['Angle of a sine'] = function (message) {
   while (sine > 0.95 || sine < 0.1) sine = crnu (Math . floor (Math . random () * 100) / 100);
   var angle = crnu (Math . asin (sine) * 180 / Math . PI, 5);
   return [
-  {action: function () {ensureSide (["S", "C"]);}, delay: 100},
+  {action: function () {ensureSide (["S", "D"]);}, delay: 100},
   {action: function () {message ("The task: find an angle, which sine is " + sine + ".");}, delay: 500},
-  {action: function () {isolate (["S", "C"]), changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 500},
-  {action: function () {message ("Move the cursor to " + sine * 10 + " on the C scale.");}, delay: 3000},
-  {action: function () {cursorTo ("C", sine * 10);}, delay: 3000},
+  {action: function () {isolate (["S", "D"]), changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 500},
+  {action: function () {message ("Move the cursor to " + sine * 10 + " on the D scale.");}, delay: 3000},
+  {action: function () {cursorTo ("D", sine * 10);}, delay: 3000},
   {action: function () {message ("Read the value of " + angle + " on the S scale.");}, delay: 3000},
-  {action: function () {cursorTo ("C", 1); dimmm (80, 255, 8);
+  {action: function () {cursorTo ("D", 1); dimmm (80, 255, 8);
   sliderules . objective = function () {
-    if (checkValue ("C", sine * 10)) {message ("Mission accomplished!"); increaseCookieResult ('Angle of a sine'); return true;}
+    if (checkValue ("D", sine * 10)) {message ("Mission accomplished!"); increaseCookieResult ('Angle of a sine'); return true;}
     return false;
   }}, delay: 6000},
   {action: function () {isolate (); message ("Try these instruction again.");}, delay: 4000}
@@ -68,15 +68,15 @@ trigonometryLessons ['Angle of a tangent'] = function (message) {
   while (tangent > 0.95 || tangent < 0.1) tangent = crnu (Math . floor (Math . random () * 100) / 100);
   var angle = crnu (Math . atan (tangent) * 180 / Math . PI, 5);
   return [
-  {action: function () {ensureSide (["T", "C"]);}, delay: 100},
+  {action: function () {ensureSide (["T", "D"]);}, delay: 100},
   {action: function () {message ("The task: find an angle, which tangent is " + tangent + ".");}, delay: 500},
-  {action: function () {isolate (["T", "C"]), changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 500},
-  {action: function () {message ("Move the cursor to " + tangent * 10 + " on the C scale.");}, delay: 3000},
-  {action: function () {cursorTo ("C", tangent * 10);}, delay: 3000},
+  {action: function () {isolate (["T", "D"]), changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 500},
+  {action: function () {message ("Move the cursor to " + tangent * 10 + " on the D scale.");}, delay: 3000},
+  {action: function () {cursorTo ("D", tangent * 10);}, delay: 3000},
   {action: function () {message ("Read the value of " + angle + " on the T scale.");}, delay: 3000},
-  {action: function () {cursorTo ("C", 1); dimmm (80, 255, 8);
+  {action: function () {cursorTo ("D", 1); dimmm (80, 255, 8);
   sliderules . objective = function () {
-    if (checkValue ("C", tangent * 10)) {message ("Mission accomplished!"); increaseCookieResult ('Angle of a tangent'); return true;}
+    if (checkValue ("D", tangent * 10)) {message ("Mission accomplished!"); increaseCookieResult ('Angle of a tangent'); return true;}
     return false;
   }}, delay: 6000},
   {action: function () {isolate (); message ("Try these instruction again.");}, delay: 4000}
