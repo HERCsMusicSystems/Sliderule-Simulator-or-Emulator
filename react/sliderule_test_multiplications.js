be532b6a9cc5fabfe1726b8bfbd9b1e560932513
robert <robert@haesemathematics.com.au>
2017-12-04 10:06:50 +1030

    Division added to simple multiplications.

--- react/sliderule_test_multiplications.js
+++ react/sliderule_test_multiplications.js
@@ -16,6 +16,20 @@ multiplicationTests ['Simple multiplication test (A/B)'] = function (message) {
   return null;
 };
 
+multiplicationTests ['Simple division test (A/B)'] = function (message) {
+  var a = rndlist ([2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  var b = rndlist ([2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  var ab = a * b;
+  message ("The task : divide " + ab + " by " + b + " using A/B scales.");
+  sliderules . objective = function () {
+    if (checkValue ("A", a) && checkValue ("B", 1)) {
+      message ("Mission accomplished!");
+      increaseCookieResult ('Simple division test (A/B)');
+      return true;
+    }
+  return false;};
+};
+
 multiplicationTests ['Simple multiplication test (C/D)'] = function (message) {
   var a = 9, b = 9;
   while (a > 10 || b > 10 || a * b > 10) {a = crnu (Math . floor (11 + Math . random () * 100) / 10); b = crnu (Math . floor (11 + Math . random () * 100) / 10);}
@@ -29,6 +43,21 @@ multiplicationTests ['Simple multiplication test (C/D)'] = function (message) {
     return false;
   };
   return null;
+};
+
+multiplicationTests ['Simple division test (C/D)'] = function (message) {
+  var a = 9, b = 9;
+  while (a > 10 || b > 10 || a * b > 10) {a = crnu (Math . floor (11 + Math . random () * 100) / 10); b = crnu (Math . floor (11 + Math . random () * 100) / 10);}
+  var ab = crnu (a * b);
+  message ("The task: divide " + ab + " by " + b + " using C/D scales.");
+  sliderules . objective = function () {
+    if (checkValue ("D", a) && checkValue ("C", 1)) {
+      message ("Mission accomplished!");
+      increaseCookieResult ('Simple division test (C/D)');
+      return true;
+    }
+    return false;
+  };
 };
 
 multiplicationTests ['Wrap-around multiplication test (C/D)'] = function (message) {
--- react/sliderule_tutorial_multiplications.js
+++ react/sliderule_tutorial_multiplications.js
@@ -27,6 +27,33 @@ multiplicationLessons ['Simple multiplication (A/B)'] = function (message) {
   ];
 };
 
+multiplicationLessons ['Simple division (A/B)'] = function (message) {
+  var a = rndlist ([2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  var b = rndlist ([2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  var ab = a * b;
+  return [
+    {action: function () {ensureSide (["A", "B"]);}, delay: 100},
+    {action: function () {message ("The task : divide " + ab + " by " + b + " using A/B scales.");}, delay: 1000},
+    {action: function () {isolate (["A", "B"]); changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 2000},
+    {action: function () {message ("Move the cursor to " + ab + " on the A scale.");}, delay: 2000},
+    {action: function () {cursorTo ("A", ab);}, delay: 2000},
+    {action: function () {message ("Align " + b + " on the B scale with the cursor.");}, delay: 4000},
+    {action: function () {slideTo ("B", b);}, delay: 2000},
+    {action: function () {message ("Move the cursor to 1 on the B scale.");}, delay: 4000},
+    {action: function () {cursorTo ("B", 1);}, delay: 1000},
+    {action: function () {message ("Then read the result " + a + " on the A scale.");}, delay: 4000},
+    {action: function () {cursorTo ("A", 1); slideTo ("B", 1); dimmm (80, 255, 8);
+    sliderules . objective = function () {
+      if (checkValue ("A", a) && checkValue ("B", 1)) {
+        message ("Mission accomplished!");
+        increaseCookieResult ('Simple division (A/B)');
+        return true;
+      }
+      return false;};}, delay: 6000},
+    {action: function () {isolate (); message ("Try these instructions again.");}, delay: 4000}
+  ];
+};
+
 multiplicationLessons ['Simple multiplication (C/D)'] = function (message) {
   var a = 9, b = 9;
   while (a > 10 || b > 10 || a * b > 10) {a = crnu (Math . floor (11 + Math . random () * 100) / 10); b = crnu (Math . floor (11 + Math . random () * 100) / 10);}
@@ -46,6 +73,33 @@ multiplicationLessons ['Simple multiplication (C/D)'] = function (message) {
       if (checkValue ("D", a * b) && checkValue ("C", b)) {
         message ("Mission accomplished!");
         increaseCookieResult ('Simple multiplication (C/D)');
+        return true;
+      }
+      return false;};}, delay: 6000},
+    {action: function () {isolate (); message ("Try these instructions again.");}, delay: 4000}
+  ];
+};
+
+multiplicationLessons ['Simple division (C/D)'] = function (message) {
+  var a = 9, b = 9;
+  while (a > 10 || b > 10 || a * b > 10) {a = crnu (Math . floor (11 + Math . random () * 100) / 10); b = crnu (Math . floor (11 + Math . random () * 100) / 10);}
+  var ab = crnu (a * b);
+  return [
+    {action: function () {ensureSide (["C", "D"]);}, delay: 100},
+    {action: function () {message ("The task: divide " + ab + " by " + b + " using C/D scales.");}, delay: 1000},
+    {action: function () {isolate (["C", "D"]); changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 2000},
+    {action: function () {message ("Move the cursor to " + ab + " on the D scale.");}, delay: 2000},
+    {action: function () {cursorTo ("D", ab);}, delay: 2000},
+    {action: function () {message ("Align " + b + " on the C scale with the cursor.");}, delay: 4000},
+    {action: function () {slideTo ("C", b);}, delay: 2000},
+    {action: function () {message ("Move the cursor to 1 on the C scale.");}, delay: 4000},
+    {action: function () {cursorTo ("C", 1);}, delay: 1000},
+    {action: function () {message ("Read the result " + a + " on the D scale.");}, delay: 4000},
+    {action: function () {cursorTo ("D", 1); slideTo ("C", 1); dimmm (80, 255, 8);
+    sliderules . objective = function () {
+      if (checkValue ("D", a) && checkValue ("C", 1)) {
+        message ("Mission accomplished!");
+        increaseCookieResult ('Simple division (C/D)');
         return true;
       }
       return false;};}, delay: 6000},
