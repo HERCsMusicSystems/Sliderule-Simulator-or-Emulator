5879135200045423c991fd8e1750b24810800e53
rama <rama@hammer.com>
2017-12-05 19:27:51 +1030

    Area of circle from radius ready.

--- react/manual/sliderule_test_circle_area.js
+++ react/manual/sliderule_test_circle_area.js
@@ -1,4 +1,19 @@
 
 var circleAreaTests = {};
 
+circleAreaTests ['Area from radius test'] = function (message) {
+  var r = 0;
+  while (r <= 1 || r > 5.6) r = crnu (1 + Math . floor (Math . random () * 111) / 10);
+  var area = crnu (Math . PI * r * r, 5);
+  message ("The task: r = " + r + ", find the area of the circle.");
+  sliderules . objective = function () {
+    if (checkValue ('A', area) && checkValue ('B', Math . PI)) {
+      message ("Mission accomplished!");
+      increaseCookieResult ('Area from radius test');
+      return true;
+    }
+    return false;
+  };
+};
+
 slideruleLessons . push (circleAreaTests);
--- react/manual/sliderule_tutorial_circle_area.js
+++ react/manual/sliderule_tutorial_circle_area.js
@@ -3,11 +3,28 @@ var circleAreaLessons = {};
 
 circleAreaLessons ['Area from radius'] = function (message) {
   var r = 0;
-  while (r <= 1 || r > 10) r = 1 + Math . floor (Math . random () * 11);
+  while (r <= 1 || r > 5.6) r = crnu (1 + Math . floor (Math . random () * 111) / 10);
   var area = crnu (Math . PI * r * r, 5);
   return [
     {action: function () {ensureSide (['A', 'B', 'D']); isolate (['A', 'B', 'D']); changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 100},
-    {action: function () {message ("The task: r = " + r + ", find the area of the circle.");}, delay: 500}
+    {action: function () {message ("The task: r = " + r + ", find the area of the circle.");}, delay: 500},
+    {action: function () {message ("Move the cursor to " + r + " on the D scale.");}, delay: 3000},
+    {action: function () {cursorTo ('D', r);}, delay: 1500},
+    {action: function () {message ("Align index (1) on the B scale with the cursor.");}, delay: 3000},
+    {action: function () {slideTo ('B', 1);}, delay: 1500},
+    {action: function () {message ("Move the cursor to \u03c0 on the B scale.");}, delay: 1500},
+    {action: function () {cursorTo ('B', Math . PI);}, delay: 1500},
+    {action: function () {message ("Read the area = " + area + " on the A scale.");}, delay: 3000},
+    {action: function () {cursorTo ('D', 1); slideTo ('B', 1); dimmm (80, 255, 8);
+    sliderules . objective = function () {
+      if (checkValue ('A', area) && checkValue ('B', Math . PI)) {
+        message ("Mission accomplished!");
+        increaseCookieResult ('Area from radius');
+        return true;
+      }
+      return false;
+    };}, delay: 6000},
+    {action: function () {isolate (); message ("Try these instructions again.");}, delay: 3000}
   ];
 };
 
