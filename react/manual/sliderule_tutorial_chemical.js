22ff11fc9e9ef3956ba621846ce0e498ff13238f
robert <robert@haesemathematics.com.au>
2017-12-07 16:00:09 +1030

    Corrections....

--- react/manual/sliderule_tutorial_chemical.js
+++ react/manual/sliderule_tutorial_chemical.js
@@ -15,7 +15,7 @@ chemicalLessons ['Element mass'] = function (message) {
     {action: function () {message ("Adjust decimal point to obtain the molecular mass of " + element . name + " = " + mass + ".");}, delay: 2000},
     {action: function () {cursorTo ('D', 1); dimmm (80, 255, 8);
     sliderules . objective = function () {
-      if (checkValue ('D', mass)) {message ("Mission accomplished!"); increaseCookieResult ('Light element mass'); return true;}
+      if (checkValue ('D', masss)) {message ("Mission accomplished!"); increaseCookieResult ('Element mass'); return true;}
       return false;
     };}, delay: 6000},
     {action: function () {isolate (); message ("Try these instructions again.");}, delay: 3000}
@@ -37,6 +37,28 @@ chemicalLessons ['Light element mass'] = function (message) {
     {action: function () {cursorTo ('D', 1); dimmm (80, 255, 8);
     sliderules . objective = function () {
       if (checkValue ('D', mass)) {message ("Mission accomplished!"); increaseCookieResult ('Light element mass'); return true;}
+      return false;
+    };}, delay: 6000},
+    {action: function () {isolate (); message ("Try these instructions again.");}, delay: 3000}
+  ];
+};
+
+chemicalLessons ['Heavy element mass'] = function (message) {
+  var element;
+  while (! element || element_mass [element . element] < 100) element = rndlist (periodic_table);
+  var mass = element_mass [element . element];
+  var masss = crnu (mass / 100);
+  return [
+    {action: function () {ensureSide (['A~M', 'N~Z', 'D']); isolate (['A~M', 'N~Z', 'D']); changeMarkings ('hairline', true); dimmm (255, 80, 8);}, delay: 100},
+    {action: function () {message ("The task: find molecular mass of " + element . name + " (" + element . element + ").");}, delay: 500},
+    {action: function () {message ("Right-click on the cursor over the Chemical scale and enter " + element . element + ".");}, delay: 2000},
+    {action: function () {cursorTo ('D', masss);}, delay: 1500},
+    {action: function () {message ("Read " + masss + " on the D scale.");}, delay: 3000},
+    {action: function () {message ("Since " + element . name + " is a heavy element, as indicated by overscore....");}, delay: 3000},
+    {action: function () {message (".... you need to adjust the decimal point to obtain the molecular mass of " + element . name + " = " + mass + ".");}, delay: 2000},
+    {action: function () {cursorTo ('D', 1); dimmm (80, 255, 8);
+    sliderules . objective = function () {
+      if (checkValue ('D', masss)) {message ("Mission accomplished!"); increaseCookieResult ('Heavy element mass'); return true;}
       return false;
     };}, delay: 6000},
     {action: function () {isolate (); message ("Try these instructions again.");}, delay: 3000}
