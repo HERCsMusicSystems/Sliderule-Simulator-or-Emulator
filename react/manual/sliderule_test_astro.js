3487400d230afeeb36bd0d04ae644a7b360a1e3f
robert <robert@haesemathematics.com.au>
2018-01-08 15:10:11 +1030

    Light year calculation added.

--- react/manual/sliderule_test_astro.js
+++ react/manual/sliderule_test_astro.js
@@ -17,5 +17,19 @@ astroTests ['Calculate parsec test'] = function (message) {
 	};
 };
 
+astroTests ['Calculate light year test'] = function (message) {
+  var speed_of_light = 299792458;
+  var year = 60 * 60 * 24 * 365.25;
+  message ("The task: calculate the length of one light year in kilometers.");
+  sliderules . objective = function () {
+    if (checkValue ('C', 2.99792) && checkValue ('D', 9.46073)) {
+      message ("Mission accomplished! One light year = 9,460,730,000,000 kilometers.");
+      increaseCookieResult ('Calculate light year test');
+      return true;
+    }
+    return false;
+  };
+};
+
 slideruleLessons . push (astroTests);
 
--- react/manual/sliderule_tutorial_astro.js
+++ react/manual/sliderule_tutorial_astro.js
@@ -11,10 +11,10 @@ astroLessons ['Calculate parsec'] = function (message) {
 		{action: function () {message ("The task: calculate the distance of one parsec in kilometers.");}, delay: 500},
 		{action: function () {message ("One parsec is the distance from wich one Astronomical Unit (distance from Earth to Sun = 149,597,870.7 km)");}, delay: 3000},
 		{action: function () {message (".... is perceived as one arcsecond.");}, delay: 2000},
-		{action: function () {message ("As tangent of one arcsecond can be approximated as \u03c0 / (180 * 60 * 60)....");}, delay: 5000},
+		{action: function () {message ("As tangent of one arcsecond can be approximated as \u03c0 / (180 \u00d7 60 \u00d7 60)....");}, delay: 5000},
 		{action: function () {message (".... one parsec is equal to one AU multiplied by reciprocal of the tangent of one arcsecond.");}, delay: 2000},
-		{action: function () {message ("1 pc = (180 * 60 * 60) / \u03c0 * " + aum + " million kilometers.");}, delay: 3000},
-		{action: function () {message ("First multiply 60 * 60. Move the cursor to 0.6 on the DF scale.");}, delay: 4000},
+		{action: function () {message ("1 pc = (180 \u00d7 60 \u00d7 60) / \u03c0 \u00d7 " + aum + " million kilometers.");}, delay: 3000},
+		{action: function () {message ("First multiply 60 \u00d7 60. Move the cursor to 0.6 on the DF scale.");}, delay: 4000},
 		{action: function () {cursorTo ('DF', 0.6);}, delay: 1000},
 		{action: function () {message ("Align 0.6 on the CIF scale with the cursor.");}, delay: 3000},
 		{action: function () {slideTo ('CIF', 0.6);}, delay: 1000},
@@ -41,6 +41,49 @@ astroLessons ['Calculate parsec'] = function (message) {
 		};}, delay: 6000},
 		{action: function () {message ("Try these instructions again.");}, delay: 1000}
 	];
+};
+
+astroLessons ['Calculate light year'] = function (message) {
+  var speed_of_light = 299792458;
+  var year = 60 * 60 * 24 * 365.25;
+  return [
+  {action: function () {ensureSide (['C', 'D']); isolate (['C', 'D']); changeMarkings ('hairline', true); dimmm (255, 40, 8);}, delay: 100},
+  {action: function () {message ("The task: calculate the length of one light year in kilometers.");}, delay: 500},
+  {action: function () {message ("Light year is the distance the light travels in vacuum in one Julian year (365.25 days).");}, delay: 3000},
+  {action: function () {message ("The speed of light = 299,792.458 kilometers per second.");}, delay: 2000},
+  {action: function () {message ("Hence the length of one light year = 60 \u00d7 60 \u00d7 24 \u00d7 365.25 \u00d7 299,792.458 kilometers.");}, delay: 2000},
+  {action: function () {message ("Move the cursor to 6 on the D scale.");}, delay: 3000},
+  {action: function () {cursorTo ('D', 6);}, delay: 1000},
+  {action: function () {message ("Align 10 on the C scale with the cursor.");}, delay: 3000},
+  {action: function () {slideTo ('C', 10);}, delay: 1000},
+  {action: function () {message ("Move the cursor to 6 on the C scale.");}, delay: 3000},
+  {action: function () {cursorTo ('C', 6);}, delay: 1000},
+  {action: function () {message ("Align 1 on the C scale with the cursor.");}, delay: 3000},
+  {action: function () {slideTo ('C', 1);}, delay: 1000},
+  {action: function () {message ("Move the cursor to 2.4 on the C scale.");}, delay: 3000},
+  {action: function () {cursorTo ('C', 2.4);}, delay: 1000},
+  {action: function () {message ("Align 10 on the C scale with the cursor.");}, delay: 3000},
+  {action: function () {slideTo ('C', 10);}, delay: 1000},
+  {action: function () {message ("Move the cursor to 3.6525 on the D scale.");}, delay: 3000},
+  {action: function () {cursorTo ('C', 3.6525);}, delay: 1000},
+  {action: function () {message ("Adjust the decimal point and read the length of one Julian year in seconds = 31,557,600");}, delay: 3000},
+  {action: function () {message ("Now multiply it by the speed of light (299,792.458 km/second).");}, delay: 3000},
+  {action: function () {message ("Align 1 on the C scale with the cursor.");}, delay: 2000},
+  {action: function () {slideTo ('C', 1);}, delay: 1000},
+  {action: function () {message ("Move the cursor to 2.99792 on the C scale.");}, delay: 3000},
+  {action: function () {cursorTo ('C', 2.99792458);}, delay: 1000},
+  {action: function () {message ("Adjust the decimal point and read the length of one light year = 9,460,730,000,000 kilometers.");}, delay: 3000},
+  {action: function () {cursorTo ('D', 1); slideTo ('D', 1); isolate (); dimmm (40, 255, 8);
+  sliderules . objective = function () {
+    if (checkValue ('C', 2.99792) && checkValue ('D', 9.46073)) {
+      message ("Mission accomplished! One light year = 9,460,730,000,000 kilometers.");
+      increaseCookieResult ('Calculate light year');
+      return true;
+    }
+    return false;
+  };}, delay: 6000},
+  {action: function () {message ("Try these instructions again.");}, delay: 1000}
+  ];
 };
 
 slideruleLessons . push (astroLessons);
