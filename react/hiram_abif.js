c330ce19423640ef61179effb62f738ce0c9a6b1
robert <robert@haesemathematics.com.au>
2018-01-10 15:24:09 +1030

    Cosmetics on Hiram Abif's artefact.

--- react/hiram_abif.js
+++ react/hiram_abif.js
@@ -1,13 +1,16 @@
 
 var sliderules = new Sliderules ();
 
+var tc = new Image (); tc . src = "templar_cross.png"; tc . onload = function () {sliderules . requireRedraw = true;};
+
 //============================
 
-var s = new Sliderule (1200); sliderules . sliderules . push (s);
+var s = new Sliderule (1200, {markings_colour: 'gold', markings_background: 'black', cursor_markings_shift: 0.002}); sliderules . sliderules . push (s);
 
-s . cursors . push (new Cursor (HairlineS, 100, 124));
-s . cursors . push (new Cursor (HairlineS, 172, 196));
-s . cursors . push (new Cursor (HairlineD, 246, 294));
+s . cursors . push (new Cursor (HairlineS, 100, 124, 'red', {marking_shift: 0.002}));
+s . cursors . push (new Cursor (HairlineS, 172, 196, 'red', {marking_shift: 0.002}));
+s . cursors . push (new Cursor (HairlineD, 246, 294, 'red', {marking_shift: 0.002}));
+s . cursors . push (new Cursor (Hairline360, 124, 172, 'red', {marking_shift: 0.002}));
 
 var r = new Rule (); s . rules . push (r);
 
@@ -38,17 +41,20 @@ r . scales . push (new scale_CF (24, {left: "DF"}));
 
 r = new Rule ({stator: 1}); s . rules . push (r);
 
+r . markings . push (new Logo (tc, -0.15, 5 * 12 + 1, 0.25));
+r . markings . push (new Logo (tc, 1.15, 5 * 12 + 1, 0.25));
+
 r . scales . push (new scale_DF (24, {left: "CF"}));
 r . scales . push (new scale_B (24, {left: "B"}));
-r . scales . push (new scale_CIF (24, {left: "DIF"}));
+r . scales . push (new scale_CIF (24, {left: "DIF", colour: 'red'}));
 r . scales . push (new spacer (2));
-r . scales . push (new scale_DI (24, {left: "CI"}));
+r . scales . push (new scale_DI (24, {left: "CI", colour: 'red'}));
 r . scales . push (new scale_C (24, {left: "C"}));
 
 r = new Rule (); s . rules . push (r);
 
 r . scales . push (new scale_D (24, {left: "D"}));
-r . scales . push (new scale_DI (24, {left: "DI"}));
+r . scales . push (new scale_DI (24, {left: "DI", colour: 'red'}));
 r . scales . push (new scale_Sdec_down (24, {left: "S"}));
 r . scales . push (new scale_TCTdec_down (24, {left: "T1"}));
 r . scales . push (new scale_TCT1dec_down (24, {left: "T2"}));
@@ -58,20 +64,23 @@ r . scales . push (new spacer (4));
 
 //===========================
 
-s = new Sliderule (1200); sliderules . sliderules . push (s);
+s = new Sliderule (1200, {markings_colour: 'gold', markings_background: 'black', cursor_markings_shift: 0.002}); sliderules . sliderules . push (s);
 
 r = new Rule (); s . rules . push (r);
 
 r . scales . push (new spacer (4));
 
-r . scales . push (new scale_L (24, {left: "L"}));
-r . scales . push (new scale_LL03 (24, {left: "LL03"}));
-r . scales . push (new scale_LL02 (24, {left: "LL02"}));
-r . scales . push (new scale_LL01 (24, {left: "LL01"}));
-r . scales . push (new scale_LL00 (24, {left: "LL00"}));
+r . scales . push (new scale_L (24, {left: "L", colour: 'red'}));
+r . scales . push (new scale_LL03 (24, {left: "LL03", colour: 'red'}));
+r . scales . push (new scale_LL02 (24, {left: "LL02", colour: 'red'}));
+r . scales . push (new scale_LL01 (24, {left: "LL01", colour: 'red'}));
+r . scales . push (new scale_LL00 (24, {left: "LL00", colour: 'red'}));
 r . scales . push (new scale_R1 (24, {left: "W1"}));
 
 r = new Rule ({stator: 1}); s . rules . push (r);
+
+r . markings . push (new Logo (tc, -0.15, 5 * 12 + 1, 0.25));
+r . markings . push (new Logo (tc, 1.15, 5 * 12 + 1, 0.25));
 
 r . scales . push (new scale_W1 (24, {left: "W1"}));
 r . scales . push (new scale_Sdec (24, {left: "S"}));
