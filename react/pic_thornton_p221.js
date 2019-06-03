46625b4f510f06249f1b1a73e32ee4c3fdd48b62
robert <robertwolf@consunet.com.au>
2019-06-04 08:37:56 +0930

    AA 010.

--- react/pic_thornton_p221.js
+++ react/pic_thornton_p221.js
@@ -10,25 +10,25 @@ var r = new Rule ({left_margin: 0.13, right_margin: 0.13}); s . rules . push (r)
 r . markings . push (new Engraving ("BRITISH", '11px times', 'left', 'black', -0.1, 18));
 r . markings . push (new Engraving ("THORNTON", '11px times', 'left', 'black', -0.1, 28));
 r . scales . push (new spacer (2));
-r . scales . push (new scale_Sdec_down (24, {left: 'S', ls: 0.03, la: la}));
-r . scales . push (new scale_STdec (24, {left: 'ST', ls: 0.03, la: la}));
+r . scales . push (new scale_Sdec_down (24, {left: 'S', ls: 0.03, la: la, right: 'sin', rs: 0.02}));
+r . scales . push (new scale_STdec (24, {left: 'ST', ls: 0.03, la: la, right: 'sin tan', rs: 0.02, draw_st_corrections: false}));
 r . scales . push (new spacer (2));
-r . scales . push (new scale_Tdec_down (24, {left: 'T', ls: 0.03, la: la}));
-r . scales . push (new scale_A (24, {left: 'A', ls: 0.03, la: la}));
+r . scales . push (new scale_Tdec_down (24, {left: 'T', ls: 0.03, la: la, right: 'tan', rs: 0.02}));
+r . scales . push (new scale_A (24, {left: 'A', ls: 0.03, la: la, right: 'X\u00b2', rs: 0.02, draw_e: false}));
 
 r = new Rule ({stator: 1, left_margin: 0.22, right_margin: 0.22}); s . rules . push (r);
-r . scales . push (new scale_B (24, {left: 'B', ls: 0.03, la: la}));
-r . scales . push (new scale_L (24, {left: 'L', ls: 0.03, la: la}));
+r . scales . push (new scale_B (24, {left: 'B', ls: 0.03, la: la, right: 'X\u00b2', rs: 0.02, draw_e: false}));
+r . scales . push (new scale_L (24, {left: 'L', ls: 0.03, la: la, right: 'log x', rs: 0.02}));
 r . scales . push (new spacer (2));
-r . scales . push (new scale_J (24, {left: 'K', ls: 0.03, la: la}));
-r . scales . push (new scale_C (24, {left: 'C', ls: 0.03, la: la}));
+r . scales . push (new scale_J (24, {left: 'K', ls: 0.03, la: la, right: 'X\u00b3', rs: 0.02, draw_e: false}));
+r . scales . push (new scale_C (24, {left: 'C', ls: 0.03, la: la, right: 'X', rs: 0.02, draw_e: false}));
 
 r = new Rule ({left_margin: 0.13, right_margin: 0.13}); s . rules . push (r);
-r . scales . push (new scale_D (24, {left: 'D', ls: 0.03, la: la}));
-r . scales . push (new scale_CI (24, {left: 'DI', ls: 0.03, la: la}));
+r . scales . push (new scale_D (24, {left: 'D', ls: 0.03, la: la, right: 'X', rs: 0.02, draw_e: false}));
+r . scales . push (new scale_CI (24, {left: 'DI', ls: 0.03, la: la, right: '1/x', rs: 0.02, marking_colour: 'red', draw_e: false}));
 r . scales . push (new spacer (2));
-r . scales . push (new scale_P_down (24, {left: 'Ps', ls: 0.03, la: la, marking_colour: 'red'}));
-r . scales . push (new scale_PT (24, {left: 'Pt', ls: 0.03, la: la, marking_colour: 'red', right_extension: 0.01}));
+r . scales . push (new scale_P_down (24, {left: 'Ps', ls: 0.03, la: la, marking_colour: 'red', right: '\u221a1-s\u00b2', rs: 0.02}));
+r . scales . push (new scale_PT (24, {left: 'Pt', ls: 0.03, la: la, right_extension: 0.01, right: '\u221a1+t\u00b2', rs: 0.02}));
 r . scales . push (new spacer (2));
 
 s = new Sliderule (1200, {left_margin: 0.22, right_margin: 0.22}); sliderules . sliderules . push (s);
@@ -42,26 +42,31 @@ r . markings . push (new Engraving ("THORNTON", '11px times', 'left', 'black', -
 r . markings . push (new Engraving ("AA 010", '11px arial', 'left', 'black', -0.1, 86));
 r . markings . push (new Engraving ("COMPREHENSIVE", '8px arial', 'left', 'black', -0.1, 96));
 r . scales . push (new spacer (2));
-r . scales . push (new scale_LL01_down (24, {left: 'LL\u2080\u2081', ls: 0.03, la: la, marking_colour: 'red'}));
-r . scales . push (new scale_LL02 (24, {left: 'LL\u2080\u2082', ls: 0.03, la: la, marking_colour: 'red'}));
+
+r . scales . push (new scale_LL01_down (24, {left: 'LL\u2080\u2081', ls: 0.03, la: la, marking_colour: 'red', right: 'e^-0.01x', rs: 0.02}));
+r . scales . push (new scale_LL02 (24, {left: 'LL\u2080\u2082', ls: 0.03, la: la, marking_colour: 'red', right: 'e^-0.1x', rs: 0.02}));
 r . scales . push (new spacer (2));
-r . scales . push (new scale_LL03_down (24, {left: 'LL\u2080\u2083', ls: 0.03, la: la, marking_colour: 'red'}));
-r . scales . push (new scale_CF (24, {left: 'DF', ls: 0.03, la: la}));
+r . scales . push (new scale_LL03_down (24, {left: 'LL\u2080\u2083', ls: 0.03, la: la, marking_colour: 'red', right: 'e^x', rs: 0.02}));
+r . scales . push (new scale_CF (24, {left: 'DF', ls: 0.03, la: la, right: '\u03c0X', rs: 0.02}));
 
 r = new Rule ({stator: 1, left_margin: 0.22, right_margin: 0.22}); s . rules . push (r);
-r . scales . push (new scale_DF (24, {left: 'CI', ls: 0.03, la: la}));
-r . scales . push (new scale_CI (24));
+r . scales . push (new scale_DF (24, {left: 'CF', ls: 0.03, la: la, right: '\u03c0X', rs: 0.02}));
+r . scales . push (new scale_CI (24, {left: 'CI', ls: 0.03, la: la, marking_colour: 'red', right: '1/x', rs: 0.02}));
 r . scales . push (new spacer (2));
 r . scales . push (new scale_ISTd_down (24, {alt: 'black', marking_alt: 'red'}));
-r . scales . push (new scale_C (24));
+r . markings . push (new Engraving ('ISd', '12px arial', 'left', 'red', 0.02, 68));
+r . markings . push (new Engraving ('ITd', '12px arial', 'left', 'black', 0.47, 68));
+r . markings . push (new Engraving ('Td', '12px arial', 'left', 'red', 0.515, 68));
+r . markings . push (new Engraving ('Sd', '12px arial', 'left', 'black', 0.97, 68));
+r . scales . push (new scale_C (24, {left: 'C', ls: 0.03, la: la, right: 'X', rs: 0.02}));
 
 r = new Rule ({left_margin: 0.13, right_margin: 0.13}); s . rules . push (r);
 r . markings . push (new Engraving ("MADE IN ENGLAND", '8px arial', 'left', 'black', -0.1, 96));
-r . markings . push (new Engraving ("\u24d2", '18px arial', 'left', 'black', 1.06, 92));
-r . scales . push (new scale_D (24));
-r . scales . push (new scale_LL3 (24));
+r . markings . push (new Engraving ("\u24d2", '18px arial', 'left', 'black', 1.07, 90));
+r . scales . push (new scale_D (24, {left: 'D', ls: 0.03, la: la, right: 'X', rs: 0.02}));
+r . scales . push (new scale_LL3 (24, {left: 'LL\u2083', ls: 0.03, la: la, right: 'e^x', rs: 0.02}));
 r . scales . push (new spacer (2));
-r . scales . push (new scale_LL2_down (24));
-r . scales . push (new scale_LL1 (24));
+r . scales . push (new scale_LL2_down (24, {left: 'LL\u2082', ls: 0.03, la: la, right: 'e^0.1x', rs: 0.02}));
+r . scales . push (new scale_LL1 (24, {left: 'LL\u2081', ls: 0.03, la: la, right: 'e^0.01x', rs: 0.02}));
 r . scales . push (new spacer (2));
 
