b1ea62ad05ac874c1bfac8adbe2df3d1c8ca93ff
rama <rama@hammer.com>
2019-06-05 22:32:48 +0930

    Work in progress on PIC....

--- react/pic_thornton_p221.js
+++ react/pic_thornton_p221.js
@@ -4,20 +4,37 @@ var model = sliderules . model;
 
 var front_german_cursors = [], front_us_cursors = [];
 
-var s = new Sliderule (1200, {left_margin: 0.22, right_margin: 0.22, cursor_top_margin: 16, cursor_bottom_margin: 16, cursor_left_extension: 0.075, cursor_right_extension: 0.075, cursor_rounding: 0, cursorHairline: 'black', hairline_top: -2, hairline_bottom: -2, cursor_limit_left: 0.035, cursor_limit_right: 0.035}); sliderules . sliderules . push (s);
-s . braces . push (new BraceSupport (0.09, 0, 298, 40, 8, 'tan', 'black'));
-s . braces . push (new BraceSupport (1.35, 0, 298, 40, 8, 'tan', 'black'));
-s . braces . push (new BezierBraces (0, 0.09, 8, 'gold', 'black', 12, 0.05, 50, 60));
+var s = new Sliderule (1200, {left_margin: 0.22, right_margin: 0.22, cursor_top_margin: model === 'pic' ? 0 : 16, cursor_bottom_margin: model === 'pic' ? 0 : 16, cursor_left_extension: 0.075, cursor_right_extension: 0.075, cursor_rounding: 0, cursorHairline: 'black', hairline_top: -2, hairline_bottom: -2, cursor_limit_left: 0.035, cursor_limit_right: 0.035}); sliderules . sliderules . push (s);
+if (model === 'pic') {
+	s . braces . push (new StaedtlerLeftBrace (4, 0.125, 0.18, 0.22, 0.22, 2, 65, 90, 'tan', 'red', 0, 8));
+//	s . braces . push (new StaedtlerLeftBrace (4, 0.133, 0.16, 0.168, 0.188, 12, 78, 90, '#ddddff', 'red'));
+	s . braces . push (new StaedtlerRightBrace (4, 0.125, 0.18, 0.22, 0.22, 2, 65, 90, 'tan', 'red', 0, 8));
+//	s . braces . push (new StaedtlerRightBrace (4, 0.133, 0.16, 0.168, 0.188, 12, 78, 90, '#ddddff', 'red'));
+} else {
+	s . braces . push (new BraceSupport (0.09, 0, 298, 40, 8, 'tan', 'black'));
+	s . braces . push (new BraceSupport (1.35, 0, 298, 40, 8, 'tan', 'black'));
+	s . braces . push (new BezierBraces (0, 0.09, 8, 'gold', 'black', 12, 0.05, 50, 60));
+}
 switch (model) {
 case 'pic':
     front_german_cursors . push (new Cursor (HairlineS, 76, 124, 'black'));
     front_german_cursors . push (new Cursor (HairlinePS, 76, 124, 'black'));
     front_us_cursors . push (new Cursor (HairlineS, 76, 124, 'black'));
     front_us_cursors . push (new Cursor (HairlineHPUS, 76, 124, 'black'));
+case 'pic':
+	s . cursorBraces . push (new CursorAngledBrace (0.076, 0.076, 18, 0, 30, 'gray'));
+    s . cursorBraces . push (new Screw (-0.05, -9, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (0.05, -9, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (-0.05, 307, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (0.05, 307, 6, Math . random () * Math . PI, 'gold', 'silver'));
+	break;
     break;
 case 'thornton':
     s . cursorGlassBraces . push (new CursorAngledBrace (0.076, 0.076, 18, -2, 0, 'gray'));
-    console . log ("Joker!");
+    s . cursorBraces . push (new Screw (-0.065, -9, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (0.065, -9, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (-0.065, 307, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (0.065, 307, 6, Math . random () * Math . PI, 'gold', 'silver'));
 case 'british thornton': s . cursors . push (new Cursor (HairlineS, 76, 124, 'black')); break;
 };
 
@@ -28,6 +45,10 @@ case 'british thornton':
     r . markings . push (new Engraving ("THORNTON", '11px times', 'left', 'black', -0.1, 28));
     break;
 case 'thornton': r . markings . push (new Engraving ("THORNTON", '11px times', 'left', 'black', -0.1, 18)); break;
+case 'pic':
+	r . markings . push (new Engraving ("PIC", '28px times', 'right', 'red', -0.073, 36));
+	r . markings . push (new VLine (-0.07, 0, 100, 'black'));
+	break;
 }
 r . scales . push (new spacer (2));
 r . scales . push (new scale_Sdec_down (24, {left: 'S', ls: 0.03, la: la, right: 'sin', rs: 0.02}));
@@ -51,10 +72,17 @@ r . scales . push (new scale_P_down (24, {left: 'Ps', ls: 0.03, la: la, marking_
 r . scales . push (new scale_PT (24, {left: 'Pt', ls: 0.03, la: la, right_extension: 0.01, right: '\u221a1+t\u00b2', rs: 0.02}));
 r . scales . push (new spacer (2));
 
-s = new Sliderule (1200, {left_margin: 0.22, right_margin: 0.22, cursor_top_margin: 16, cursor_bottom_margin: 16, cursor_left_extension: 0.075, cursor_right_extension: 0.075, cursor_rounding: 0, cursorHairline: 'black', hairline_top: -2, hairline_bottom: -2, cursor_limit_left: 0.035, cursor_limit_right: 0.035}); sliderules . sliderules . push (s);
-s . braces . push (new BraceSupport (0.09, 0, 298, 40, 8, 'tan', 'black'));
-s . braces . push (new BraceSupport (1.35, 0, 298, 40, 8, 'tan', 'black'));
-s . braces . push (new BezierBraces (0, 0.09, 8, 'gold', 'black', 12, 0.05, 50, 60));
+s = new Sliderule (1200, {left_margin: 0.22, right_margin: 0.22, cursor_top_margin: model === 'pic' ? 0 : 16, cursor_bottom_margin: model === 'pic' ? 0 : 16, cursor_left_extension: 0.075, cursor_right_extension: 0.075, cursor_rounding: 0, cursorHairline: 'black', hairline_top: -2, hairline_bottom: -2, cursor_limit_left: 0.035, cursor_limit_right: 0.035}); sliderules . sliderules . push (s);
+if (model === 'pic') {
+	s . braces . push (new StaedtlerLeftBrace (4, 0.125, 0.18, 0.22, 0.22, 2, 65, 90, 'tan', 'red', 0, 8));
+//	s . braces . push (new StaedtlerLeftBrace (4, 0.133, 0.16, 0.168, 0.188, 12, 78, 90, '#ddddff', 'red'));
+	s . braces . push (new StaedtlerRightBrace (4, 0.125, 0.18, 0.22, 0.22, 2, 65, 90, 'tan', 'red', 0, 8));
+//	s . braces . push (new StaedtlerRightBrace (4, 0.133, 0.16, 0.168, 0.188, 12, 78, 90, '#ddddff', 'red'));
+} else {
+	s . braces . push (new BraceSupport (0.09, 0, 298, 40, 8, 'tan', 'black'));
+	s . braces . push (new BraceSupport (1.35, 0, 298, 40, 8, 'tan', 'black'));
+	s . braces . push (new BezierBraces (0, 0.09, 8, 'gold', 'black', 12, 0.05, 50, 60));
+}
 
 r = new Rule ({left_margin: 0.13, right_margin: 0.13}); s . rules . push (r);
 switch (model) {
@@ -66,9 +94,27 @@ case 'british thornton':
 case 'thornton':
     r . markings . push (new Engraving ("THORNTON", '11px times', 'left', 'black', -0.1, 18));
     r . markings . push (new Engraving ("No P 221", '11px arial', 'left', 'black', -0.1, 86));
+    s . cursorGlassBraces . push (new CursorAngledBrace (0.076, 0.076, 18, -2, 0, 'gray'));
+    s . cursorBraces . push (new Screw (-0.065, -9, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (0.065, -9, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (-0.065, 307, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (0.065, 307, 6, Math . random () * Math . PI, 'gold', 'silver'));
     break;
+case 'pic':
+	r . markings . push (new Engraving ("\u24d2", '26px arial', 'left', 'black', 1.08, 30));
+	s . cursorBraces . push (new CursorAngledBrace (0.076, 0.076, 18, 0, 30, 'gray'));
+    s . cursorBraces . push (new Screw (-0.05, -9, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (0.05, -9, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (-0.05, 307, 6, Math . random () * Math . PI, 'gold', 'silver'));
+    s . cursorBraces . push (new Screw (0.05, 307, 6, Math . random () * Math . PI, 'gold', 'silver'));
+	break;
 }
-r . markings . push (new Engraving ("COMPREHENSIVE", '8px arial', 'left', 'black', -0.1, 96));
+if (model === 'pic') {
+	r . markings . push (new Engraving ("PIC", '28px times', 'right', 'red', -0.073, 36));
+    r . markings . push (new Engraving ("No. 221", '11px arial', 'right', 'red', -0.073, 86));
+    r . markings . push (new Engraving ("England", '9px arial', 'right', 'red', -0.073, 96));
+	r . markings . push (new VLine (-0.07, 0, 100, 'black'));
+} else r . markings . push (new Engraving ("COMPREHENSIVE", '8px arial', 'left', 'black', -0.1, 96));
 r . scales . push (new spacer (2));
 
 r . scales . push (new scale_LL01_down (24, {left: 'LL\u2080\u2081', ls: 0.03, la: la, marking_colour: 'red', right: 'e^-0.01x', rs: 0.02}));
@@ -89,8 +135,10 @@ r . markings . push (new Engraving ('Sd', '12px arial', 'left', 'black', 0.97, 6
 r . scales . push (new scale_C (24, {left: 'C', ls: 0.03, la: la, right: 'X', rs: 0.02}));
 
 r = new Rule ({left_margin: 0.13, right_margin: 0.13}); s . rules . push (r);
-r . markings . push (new Engraving ("MADE IN ENGLAND", '8px arial', 'left', 'black', -0.1, 96));
-r . markings . push (new Engraving ("\u24d2", '18px arial', 'left', 'black', 1.07, 90));
+if (model != 'pic') {
+	r . markings . push (new Engraving ("MADE IN ENGLAND", '8px arial', 'left', 'black', -0.1, 96));
+	r . markings . push (new Engraving ("\u24d2", '18px arial', 'left', 'black', 1.07, 90));
+}
 r . scales . push (new scale_D (24, {left: 'D', ls: 0.03, la: la, right: 'X', rs: 0.02}));
 r . scales . push (new scale_LL3 (24, {left: 'LL\u2083', ls: 0.03, la: la, right: 'e^x', rs: 0.02}));
 r . scales . push (new spacer (2));
