8790bdfc0a76379efcd01b38dbeadc67f33caa63
robert <robertwolf@consunet.com.au>
2019-07-16 14:25:01 +0930

    LL2M !

--- react/folded_scales.js
+++ react/folded_scales.js
@@ -25,7 +25,7 @@
 // CF36, DF36, CIF36, DIF36
 // CFM, DFM, CIFM, DIFM
 // CF1M, DF1M, CIF1M, DIF1M
-// LL2M
+// LL2M(_down)
 //////////////////////////////////////////////////////////////////////////////
 
 var scale_CF10 = function (height, options) {
@@ -242,6 +242,30 @@ var scale_DIF1M = function (height, options) {
   return s;
 };
 
+var draw_LL2M = function (ctx, length, height, s) {
+  var limit = 1 + s . right_extension;
+	var h5 = height * 0.5; var h2 = height * 0.2; var h3 = height * 0.3; var h4 = height * 0.4;
+  draw_MRS (ctx, s . location, length, 2, 3, 0.5, limit, h5);
+  draw_MRS (ctx, s . location, length, 4, 20, 1, limit, h5);
+  draw_XR (ctx, s . location, length, 2, 2.5, limit, h4, 0.5, 0.1, 0.5);
+  draw_XR (ctx, s . location, length, 2.5, 20, limit, h3, 0.5, 0.1, 0.5);
+  draw_XR (ctx, s . location, length, 2.5, 4, limit, h2, 0.1, 0.02, 0.1);
+  draw_XR (ctx, s . location, length, 3, 20, limit, h4, 1, 0.5, 1);
+  limit = - s . left_extension;
+  draw_MLSF (ctx, s . location, length, 1.1, 1.45, 0.05, 2, limit, h5);
+	draw_MLS (ctx, s . location, length, 1.5, 1.9, 0.1, limit, h5);
+  draw_XL (ctx, s . location, length, 1.5, 2.5, limit, h3, 0.1, 0.05, 0.1);
+  draw_XL (ctx, s . location, length, 1.8, 2.5, limit, h2, 0.05, 0.01, 0.05);
+  draw_XL (ctx, s . location, length, 1.4, 1.8, limit, h3, 0.05, 0.01, 0.05);
+  draw_XL (ctx, s . location, length, 1.4, 1.8, limit, h2, 0.01, 0.005, 0.01);
+  draw_XL (ctx, s . location, length, 1.1, 1.4, limit, h3, 0.05, 0.01, 0.05);
+  draw_XL (ctx, s . location, length, 1.1, 1.4, limit, h2, 0.01, 0.002, 0.01);
+};
+
 var scale_LL2M = inherit (spacer);
 scale_LL2M . prototype . value = function (location) {return Math . pow (Math . E, Math . pow (10, location - 1) / Math . log10 (Math . E));};
 scale_LL2M . prototype . location = function (value) {return 1 + Math . log10 (Math . log (value) * Math . log10 (Math . E));};
+scale_LL2M . prototype . draw = function (ctx, length) {ctx . translate (0, this . height); draw_LL2M (ctx, length, this . height, this);};
+
+var scale_LL2M_down = inherit (scale_LL2M);
+scale_LL2M_down . prototype . draw = function (ctx, length) {draw_LL2M (ctx, length, - this . height, this);};
